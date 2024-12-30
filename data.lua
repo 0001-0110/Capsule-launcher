7194f2bd774eafc10f76a1809da51accc3243725
Remi TROTEL <remi.trotel@epita.fr>
2024-12-30 17:01:49 +0100

    Added comments

--- data.lua
+++ data.lua
@@ -7,16 +7,19 @@ local distractor_capsule = require("prototypes.capsules.distractor_capsule")
 local destroyer_capsule = require("prototypes.capsules.destroyer_capsule")
 local capsule_launcher = require("prototypes.turrets.capsule-launcher")
 
-local function load(machin)
-    machin.update_vanilla_technology()
-    data:extend(machin.prototypes)
+local function load(capsule)
+    capsule.update_vanilla_technology()
+    data:extend(capsule.prototypes)
 end
 
+-- Add the new ammo category
 data:extend({ ammo_category, })
 
+-- Load all prototypes related to the capsule launcher
+data:extend(capsule_launcher.prototypes)
+
+-- Load all prototypes for the different ammo that can be used inside the capsule launcher
 --load(effect_capsules)
 --load(defender_capsule)
 load(distractor_capsule)
 --load(destroyer_capsule)
-
-data:extend(capsule_launcher.prototypes)
--- prototypes/capsules/combat_robot_capsule.lua
+++ prototypes/capsules/combat_robot_capsule.lua
@@ -6,6 +6,9 @@ local BASED_ON = "rocket"
 local function create_projectile_prototype(name, entity_count)
     local projectile = table.deepcopy(data.raw["projectile"][BASED_ON])
     projectile.name = utils.prefix(name .. "-projectile")
+    -- Set the effect on impact
+    -- Since the only effect is to create an entity, this projectile will deal no damage
+    -- TODO Add more effects on impact (sounds, particles...)
     projectile.action.action_delivery =
     {
         type = "instant",
@@ -15,6 +18,8 @@ local function create_projectile_prototype(name, entity_count)
                 type = "create-entity",
                 entity_name = name,
                 -- TODO Change this according to entity count
+                -- The number of offsets will dictate the number of entities created
+                -- Each offset is a position relative to the impact point of the projectile
                 offsets = { {0, 0}, {1, 1}, {1, -1}, }
             },
         },
@@ -25,7 +30,9 @@ end
 local function create_item_prototype(name, projectile)
     local item = table.deepcopy(data.raw["ammo"][BASED_ON])
     item.name = utils.prefix(name .. "-capsule")
+    -- Set the ammo category to allow capsule launchers to use this as ammo
     item.ammo_category = ammo_category.name
+    -- Set the projectile to fire when this item is used in the turret
     item.ammo_type.action.action_delivery.projectile = projectile.name
     return item
 end
@@ -53,7 +60,9 @@ local function create_recipe_prototype(name, result_item)
 end
 
 local function create_technology_update(name, recipe)
+    -- Return a function to avoid side effects
     return function (a)
+        -- Update the existing technology effects to add the new recipe to unlock
         table.insert(data.raw["technology"][name].effects, { type = "unlock-recipe", recipe = recipe.name, })
     end
 end
